5806555a3df40f6fbf6211202950eaba054ebbd8
John Nguyen <jngu@yelp.com>
2016-09-15 11:58:58 -0700

    Fill out user information on dmv page

--- src/dmv.py
+++ src/dmv.py
@@ -11,6 +11,24 @@ def get_dmv_appointment_html():
     link_elem = browser.find_element_by_link_text('Office Visit Appointment')
     link_elem.click()
 
+    # Choose DMV Office
+    select_office = Select(browser.find_element_by_name('officeId'))
+    select_office.select_by_visible_text('SAN FRANCISCO')
+
+    # Set number of items to process
+    items_to_process = browser.find_element_by_id('one_task').click()
+
+    # Choose reasons for appointment
+    browser.find_element_by_id('taskDLO').click()
+
+    # Enter personal information
+    browser.find_element_by_id('first_name').send_keys('first')
+    browser.find_element_by_id('last_name').send_keys('last')
+    browser.find_element_by_id('area_code').send_keys('555')
+    browser.find_element_by_name('telPrefix').send_keys('555')
+    browser.find_element_by_name('telSuffix').send_keys('5555')
+
+    browser.find_element_by_name('ApptForm').submit()
     return browser.page_source
 
 
