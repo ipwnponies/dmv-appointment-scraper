1d173a65e54bec680f14a9c91c41c286f28dd590
John Nguyen <ipwnponies@gmail.com>
2019-04-20 11:48:42 -0700

    Switch to using strictyaml
    Switch to strictyaml instead of PyYaml. This fixes the use of dangerous
    load and adds schema validation.

--- dmv_appointment_scraper/config.py
+++ dmv_appointment_scraper/config.py
@@ -1,16 +1,40 @@
 '''
 Utilties to read config.yaml.
 '''
-import os
+from pathlib import Path
 
-import yaml
+from strictyaml import load
+from strictyaml import Map
+from strictyaml import Str
+from strictyaml import UniqueSeq
+from strictyaml import Url
+
+CONFIG_SCHEMA = Map({
+    'mail': Map({
+        'recipients': UniqueSeq(Str()),
+        'sender': Str(),
+        'subject': Str(),
+        'password': Str(),
+    }),
+    'dmv': Map({
+        'welcome_url': Url(),
+        'user': Map({
+            'first_name': Str(),
+            'last_name': Str(),
+            'area_code': Str(),
+            'phone_prefix': Str(),
+            'phone_suffix': Str(),
+        }),
+        'office': Str(),
+        'task': Str(),
+    }),
+})
 
 
 def read_config():
     '''Load yaml config'''
-
-    with open(os.path.join(os.path.dirname(__file__), 'config.yaml'), 'r') as config_file:
-        return yaml.load(config_file)
+    config_file = Path(__file__).parent / 'config.yaml'
+    return load(config_file.read_text(), CONFIG_SCHEMA).data
 
 
 CONFIG = read_config()
